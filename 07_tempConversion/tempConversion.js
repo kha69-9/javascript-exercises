767c0c7d0c7758e478ba284c9fd727c1040b716f
kha69-9 <maniwannasee@outlook.com>
2024-12-07 23:55:48 +0200

    solve problems 6 and 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  result = (tempInFahrenheit - 32) / 1.8
+  return +result.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  result = (tempInCelsius * 1.8) + 32
+  return +result.toFixed(1);
+   
 };
 
 // Do not edit below this line
