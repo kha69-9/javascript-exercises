bb4e28a378ffc3914ebb4fa69d1d4b139705cf09
kha69-9 <maniwannasee@outlook.com>
2024-12-07 23:29:26 +0200

    edit solution to problem 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -6,19 +6,19 @@ const sumAll = function(num1, num2) {
         Math.trunc(num2) !== num2) {
         return 'ERROR';
     }
-
+ 
     if (num1 <= num2) {
-        for (let i = num1; i <= num2; i++) {
-            sum += i;
-        }
-        return sum;
-    } else if (num2 < num1) {
-        for (let i = num2; i <= num1; i++){
-            sum += i;
-        }
-        return sum;
+        max = num2;
+        min = num1;
+    } else {
+        max = num1;
+        min = num2;
     }
-    
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+        return sum;
 };
 
 // Do not edit below this line
