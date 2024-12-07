66e1b45f588cd2e2ad7fab44790073acc7933389
kha69-9 <maniwannasee@outlook.com>
2024-12-07 04:01:20 +0200

    solve exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatations) {
+    if (repeatations < 0) return 'ERROR'
+    let stringHolderArray = [];
+    for (let i=0; i < repeatations; i++) {
+        stringHolderArray.push(string);
+    }
+    return stringHolderArray.join('');
 };
 
 // Do not edit below this line
